2848a7565cae26d545fd250258eee6be9313f248
EstieBlue <estieblue67@gmail.com>
2026-07-03 14:04:31 -0500

    Added comments to the files about their purpose

---
+++ Algorithms/BFS.py
@@ -0,0 +1,5 @@
+# Using the breath-first search algorithm to traverse a graph
+# This implementation uses a queue to explore nodes level by level.
+# The BFS algorithm starts at a given node (the "root" node) and explores all its neighbors before moving on to the neighbors' neighbors. 
+# This process continues until all reachable nodes have been visited.
+
---
+++ main.py
@@ -0,0 +1,3 @@
+# This is the main file for the BFS implementation
+# Where essentially the robots are created and the BFS algorithm is executed to find the shortest path in a graph.
+# Without the multiple robots colliding with each other, the BFS algorithm will find the shortest path for each robot to reach its destination.
